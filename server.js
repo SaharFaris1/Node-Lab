39ef0b5d46b6d0604d09c60d4ceede79ad848a4f
Sahar Faris <saharfaris@Sahars-Laptop.local>
2025-06-15 13:15:34 +0300

    update

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import { createServer } from 'http'
-import { readFile } from 'fs'
+import fs from "fs";
+
+
+const data = fs.readFileSync("./person.jpeg");
 const server = createServer((req, res) => {
-  readFile('person.jpeg', (_, data) => {
-    res.writeHead(200, { 
-        'Content-Type':'image/jpeg' })
+   res.writeHead(200, {'Content-Type':'image/jpeg' })
     res.end(data)
-  })})
+  })
 server.listen(3000, '127.0.0.1', () => {
   console.log('Listening on http://127.0.0.1:3000')
 })
